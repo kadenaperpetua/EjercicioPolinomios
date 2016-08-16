9a8b0e574a96cdf075100ba24116b3afd45b65ff
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-16 15:15:08 -0500

    ELI: Declara los metodos de la clase polinomio

---
+++ src/com/eli/polinomios/Polinomio.java
@@ -0,0 +1,28 @@
+package com.eli.polinomios;
+
+public class Polinomio {
+    public Polinomio(double[] coeficientes){
+
+    }
+
+    public Polinomio sumarPolinomios(Polinomio sumador){
+        return sumador;
+    }
+
+    public Polinomio multiplicarCoeficiente(double coeficiente){
+        return this;
+    }
+
+    public Polinomio derivarPolinomio(){
+        return this;
+    }
+
+    public double evaluarPolinomio(){
+        return 0.0;
+    }
+
+    public void visualizarPolinomio(){
+
+    }
+
+}
