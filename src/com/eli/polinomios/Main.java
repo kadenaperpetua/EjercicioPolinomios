8eaffb5f9bcbb5eb3e21b0b123976084f8c6638d
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-16 15:56:10 -0500

    ELI: Escribe metodo evaluarPolinomio

--- src/com/eli/polinomios/Main.java
+++ src/com/eli/polinomios/Main.java
@@ -3,6 +3,10 @@ package com.eli.polinomios;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        double[] entrada ={0};
+        Polinomio eli = new Polinomio(entrada);
+        eli.visualizarPolinomio();
+        System.out.println(eli.evaluarPolinomio(1));
     }
 }
--- src/com/eli/polinomios/Polinomio.java
+++ src/com/eli/polinomios/Polinomio.java
@@ -1,31 +1,47 @@
 package com.eli.polinomios;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Polinomio {
 
     private double[] coeficientes;
 
-    public Polinomio(double[] coeficientes){
-        this.coeficientes=coeficientes;
+    public Polinomio(double[] coeficientes) {
+        this.coeficientes = coeficientes;
     }
 
-    public Polinomio sumarPolinomios(Polinomio sumador){
+    public Polinomio sumarPolinomios(Polinomio sumador) {
         return sumador;
     }
 
-    public Polinomio multiplicarCoeficiente(double coeficiente){
+    public Polinomio multiplicarCoeficiente(double coeficiente) {
         return this;
     }
 
-    public Polinomio derivarPolinomio(){
+    public Polinomio derivarPolinomio() {
         return this;
     }
 
-    public double evaluarPolinomio(){
-        return 0.0;
+    public double evaluarPolinomio(double valorDeX) {
+        double valorNumerico = 0;
+        for (double coeficiente : coeficientes) {
+            valorNumerico += coeficiente * valorDeX;
+        }
+        return valorNumerico;
     }
 
-    public void visualizarPolinomio(){
+    public void visualizarPolinomio() {
 
+        System.out.print("P(x) = ");
+        for (int i = 0; i < coeficientes.length - 1; i++) {
+            int indice = coeficientes.length - i - 1;
+            if (coeficientes[i] != 0) {
+                System.out.print(coeficientes[i] + " x^" + indice + " + ");
+            }
+        }
+        System.out.print(coeficientes[(coeficientes.length - 1)]);
+        System.out.println();
     }
-
 }
+
+
